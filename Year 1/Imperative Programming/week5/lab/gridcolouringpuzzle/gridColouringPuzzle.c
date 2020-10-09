97b0d3159f9960057c9c546807cadc5646f26287
Matej <matejxpriesol@gmail.com>
2020-10-09 17:59:26 +0200

    Added lab 5

---
+++ Year 1/Imperative Programming/week5/lab/gridcolouringpuzzle/gridColouringPuzzle.c
@@ -0,0 +1,16 @@
+/* file    : gridColouringPuzzle.c */
+/* author  : Matej Priesol (email: m.priesol@student.rug.nl) */
+/* date    : TUE Oct 6 2020 */
+/* version : 1.0 */
+
+/* Description:  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+
+
+    return 0;
+}
