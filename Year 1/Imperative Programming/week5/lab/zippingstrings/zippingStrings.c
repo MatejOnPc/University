6d2906427724324f0cde75b21e6b8b8463abb2f2
Matej <matejxpriesol@gmail.com>
2020-10-11 23:46:07 +0200

    worked on exer 2

--- Year 1/Imperative Programming/week5/lab/zippingstrings/zippingStrings.c
+++ Year 1/Imperative Programming/week5/lab/zippingstrings/zippingStrings.c
@@ -9,8 +9,47 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+/* This function creates dynamically allocated array, with size given as a parameter */
+int *dynamicArray(int size) {
+	int *array = malloc(size*sizeof(int));
+	return array;
+}
 
+/* This function calculates square root of n, used for lenght of an array */
+int squareRoot(int n) {
+    int num = 0;
+    for (int i = 1; i*i<=n; i++) {
+        num++;
+    }
+    return num;
+}
+
+/* recursive function */
+int merge(int x, int y, char *arr, int i, char *a1, char *a2, int lenght1, int lenght2) {
+    arr[i] = a1[x];
+
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    int n;    /* number of auxiliary letters */
+    char *array1[33], *array2[33], *arrayGoal[34];    /* arrays for strings, lenght is 33 because I need to count for '\0' */
+    char *arrayMerged[33];    /* for saving the merged words */
+    int square;
+    int output;
+    int lenght1, lenght2;
+
+    scanf("%d", &n);
+    scanf("%s", array1);
+    scanf("%s", array2);
+    scanf("%s", arrayGoal);
+
+    lenght1 = strlen(array1);
+    lenght2 = strlen(array2);
+
+    output = merge(0, 0, arrayMerged, 0, lenght1, lenght2);    /* calling the recursive function */
+
+    printf("%d\n", output);
 
     return 0;
 }
