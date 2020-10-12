a7f5a83e8021c0bc5d3ed8d72b6a6ddccfb9ac57
Matej <matejxpriesol@gmail.com>
2020-10-12 16:02:59 +0200

    worked on linear equations

--- Year 1/Imperative Programming/week5/lab/linearequation/linearEquation.c
+++ Year 1/Imperative Programming/week5/lab/linearequation/linearEquation.c
@@ -9,7 +9,58 @@
 #include <stdlib.h>
 #include <string.h>
 
+int fillArray(int a[], int lenght) {
+    int x;
+    for (int i = 0; i < lenght; i++) {
+        scanf("%d", &x);
+        a[i] = x;
+    }
+}
+
+int findSolutions(int y, int *a, int *mul, int lenght, int index) {
+    int sum = 0;
+    int steps = 0;
+
+    for (int i = 0; i < lenght; i++) {
+        sum += a[i] * mul[i];
+    }
+
+    if (sum > y) {
+        mul[index] = 0;
+        steps--;
+        mul[index-steps]++;
+    }
+
+    if (sum == y) {
+        mul[index] = 0;
+        index++;
+        findSolutions();
+        return 1;
+    }
+
+    mul[index]++;
+    findSolutions();
+
+}
+
 int main(int argc, char *argv[]) {
+    int solutions = 0;
+    int n;
+    int array[n];
+    int multiples[n];
+    int y;
+    
+    scanf("%d:", &n);
+    fillArray(array, n);
+    scanf("%d",&y);
+
+    for (int i = 0; i < n; i++) {
+        multiples[i] = 0;
+    }
+
+
+
+    solutions = findSolutions(y, 0, 0);    
 
 
     return 0;
