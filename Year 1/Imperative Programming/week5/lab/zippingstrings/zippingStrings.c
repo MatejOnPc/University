37eb7eac4ba65defb99a214cbb39e566c4334a8f
Matej <matejxpriesol@gmail.com>
2020-10-12 16:49:51 +0200

    worked on zippingStrings

--- Year 1/Imperative Programming/week5/lab/zippingstrings/zippingStrings.c
+++ Year 1/Imperative Programming/week5/lab/zippingstrings/zippingStrings.c
@@ -9,33 +9,44 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* This function creates dynamically allocated array, with size given as a parameter */
-int *dynamicArray(int size) {
-	int *array = malloc(size*sizeof(int));
-	return array;
-}
-
-/* This function calculates square root of n, used for lenght of an array */
-int squareRoot(int n) {
-    int num = 0;
-    for (int i = 1; i*i<=n; i++) {
-        num++;
-    }
-    return num;
-}
 
 /* recursive function */
-int merge(int x, int y, char *arr, int i, char *a1, char *a2, int lenght1, int lenght2) {
-    arr[i] = a1[x];
+int merge(int x, int y, int i, int n, char *aM, char *aG, char *a1, char *a2, int lenght1, int lenght2) {
+    if (x-1 == lenght1) {
+        return 0;
+    }
+    
+    printf("i = %d, aM = %s\n", i, aM);
 
-    return;
+    if (y-1 == lenght2) {
+        return 0;
+    }
+    
+    aM[i] = a1[x];
+
+    if (aM[i] != aG[i]) {
+        if (n > 0) {
+            aM[i] = aG[i];
+            n--;
+            return merge(x, y, i, n, aM, aG, a1, a2, lenght1, lenght2) + merge(y, x, i, n, aM, aG, a2, a1, lenght2, lenght1);
+        } else {
+            return 0;
+        }
+    }
+
+    i++;
+
+    if (aM == aG) {
+        return 1;
+    }
+
+    return merge(x+1, y, i, n, aM, aG, a1, a2, lenght1, lenght2) + merge(y, x, i, n, aM, aG, a2, a1, lenght2, lenght1);
 }
 
 int main(int argc, char *argv[]) {
     int n;    /* number of auxiliary letters */
-    char *array1[33], *array2[33], *arrayGoal[34];    /* arrays for strings, lenght is 33 because I need to count for '\0' */
-    char *arrayMerged[33];    /* for saving the merged words */
-    int square;
+    char array1[33], array2[33], arrayGoal[34];    /* arrays for strings, lenght is 33 because I need to count for '\0' */
+    char arrayMerged[33];    /* for saving the merged words */
     int output;
     int lenght1, lenght2;
 
@@ -44,10 +55,12 @@ int main(int argc, char *argv[]) {
     scanf("%s", array2);
     scanf("%s", arrayGoal);
 
+    printf("%d, %s, %s, %s\n",  n, array1, array2, arrayGoal);
+
     lenght1 = strlen(array1);
     lenght2 = strlen(array2);
 
-    output = merge(0, 0, arrayMerged, 0, lenght1, lenght2);    /* calling the recursive function */
+    output = merge(0, 0, 0, n, arrayMerged, arrayGoal, array1, array2, lenght1, lenght2) + merge(0, 0, 0, n, arrayMerged, arrayGoal, array2, array1, lenght2, lenght1);    /* calling the recursive function */
 
     printf("%d\n", output);
 
