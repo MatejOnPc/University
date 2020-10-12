04d42e8817d4861c63a0759421da3c2177c3ca5d
Matej <matejxpriesol@gmail.com>
2020-10-12 19:03:05 +0200

    Tried different approach for zippingStrings

--- Year 1/Imperative Programming/week5/lab/zippingstrings/zippingStrings.c
+++ Year 1/Imperative Programming/week5/lab/zippingstrings/zippingStrings.c
@@ -11,36 +11,57 @@
 
 
 /* recursive function */
-int merge(int x, int y, int i, int n, char *aM, char *aG, char *a1, char *a2, int lenght1, int lenght2) {
-    if (x-1 == lenght1) {
-        return 0;
-    }
-    
-    printf("i = %d, aM = %s\n", i, aM);
+int merge(int i, int n, char *aM, char *aG, char *a1, char *a2, int lenght1, int lenght2) {
+    /* char newString[33]; */
+    printf("aM = %s\n", aM);
+    printf("aG = %s\n", aG);
 
-    if (y-1 == lenght2) {
-        return 0;
+    if (aG == aM) {
+        return 1;
     }
-    
-    aM[i] = a1[x];
 
-    if (aM[i] != aG[i]) {
+    printf("aG[i] = %c, a1[0] = %c and a2[0] = %c\n", aG[i], a1[0], a2[0]);
+    if ((aG[i] == a1[0]) && (lenght1 >= 0)) {
+        aM[i] = a1[0];
+        printf("aM[i] = %c\n", aM[i]);
+        for (int j = 0; j < lenght1 - 1; j++) {
+            a1[j] = a1[j+1];
+        }
+        a1[lenght1-1] = '\0';
+        printf("new string = %s\n", a1);
+        lenght1--;
+        i++;
+        printf("lenght1 = %d\n", lenght1);
+        return merge(i, n, aM, aG, a1, a2, lenght1, lenght2);
+    }
+
+    if ((aG[i] == a2[0]) && (lenght2 >= 0)) {
+        aM[i] = a2[0];
+        printf("aM[i] = %c\n", aM[i]);
+        for (int j = 0; j < lenght2 - 1; j++) {
+            a2[j] = a2[j+1];
+        }
+        a2[lenght2-1] = '\0';
+        printf("new string = %s\n", a2);
+        lenght2--;
+        i++;
+        printf("lenght2 = %d\n", lenght2);
+        return merge(i, n, aM, aG, a1, a2, lenght1, lenght2);
+    }
+
+    if ((aG[i] != a1[0]) && (aG[i] != a2[0])) {
         if (n > 0) {
             aM[i] = aG[i];
             n--;
-            return merge(x, y, i, n, aM, aG, a1, a2, lenght1, lenght2) + merge(y, x, i, n, aM, aG, a2, a1, lenght2, lenght1);
+            i++;
+            return merge(i, n, aM, aG, a1, a2, lenght1, lenght2);
         } else {
             return 0;
         }
     }
-
-    i++;
-
-    if (aM == aG) {
-        return 1;
-    }
-
-    return merge(x+1, y, i, n, aM, aG, a1, a2, lenght1, lenght2) + merge(y, x, i, n, aM, aG, a2, a1, lenght2, lenght1);
+    return 0;
+    /*
+    printf("i = %d, aM = %s, a1 = %s, n = %d\n", i, aM, a1, n); */
 }
 
 int main(int argc, char *argv[]) {
@@ -60,7 +81,8 @@ int main(int argc, char *argv[]) {
     lenght1 = strlen(array1);
     lenght2 = strlen(array2);
 
-    output = merge(0, 0, 0, n, arrayMerged, arrayGoal, array1, array2, lenght1, lenght2) + merge(0, 0, 0, n, arrayMerged, arrayGoal, array2, array1, lenght2, lenght1);    /* calling the recursive function */
+    output = merge(0, n, arrayMerged, arrayGoal, array1, array2, lenght1, lenght2);
+    /* + merge(0, n, arrayMerged, arrayGoal, array2, array1, lenght2, lenght1); */   /* calling the recursive function */
 
     printf("%d\n", output);
 
