2755835e5300ac788563b4385e31374eae5c6b4f
Matej <matejxpriesol@gmail.com>
2020-10-13 13:00:39 +0200

    Kind of works
    The isssue about dumped core was my memory, however it fails test case 10 because it's inefficient

--- Year 1/Imperative Programming/week5/lab/linearequation/linearEquation.c
+++ Year 1/Imperative Programming/week5/lab/linearequation/linearEquation.c
@@ -1,14 +1,14 @@
 /* file    : linearEquation.c */
 /* author  : Matej Priesol (email: m.priesol@student.rug.nl) */
 /* date    : TUE Oct 6 2020 */
-/* version : 1.0 */
+/* version : 1.2 */
 
-/* Description:  */
+/* Description: This program finds number of possible solutions for a given linear equation */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+/* fills array from input */
 void fillArray(int a[], int lenght) {
     int x;
     for (int i = 0; i < lenght; i++) {
@@ -17,26 +17,37 @@ void fillArray(int a[], int lenght) {
     }
 }
 
+/* fills array multiples */
+void fillMultiples(int a[], int lenght) {
+    for (int i = 0; i < lenght; i++) {
+        a[i] = 0;
+    }
+}
+
+/* fills maximum values for each integer, used for going through all the possible solutions */
 void fillMaxMultiples(int max[], int a[], int lenght, int y) {
     for (int i = 0; i < lenght; i++) {
         max[i] = y / a[i];
     }
 }
 
+/* recursive funcion */
 int findSolutions(int y, int a[], int mul[], int max[], int lenght, int solutions) {
     int sum = 0;
     int i;
 
-    for (int i = 0; i < lenght; i++) {
-        printf("%d ", mul[i]);
-    }
-    printf("\n");
-
-    if (mul[0] > max[0]) {
+    /* base case */
+    if (mul[1] > max[1]) {
         return solutions;
     }
 
-    for (i = 0; i < lenght; i++) {
+    /* calculates sum */
+    for (i = 1; i < lenght; i++) {
+        sum += a[i] * mul[i];
+    }
+
+    /* makes sure I move through all possibilities */
+    for (i = 1; i < lenght; i++) {
         if (mul[i] > max[i]) {
             mul[i-1]++;
             mul[i] = 0;
@@ -44,23 +55,19 @@ int findSolutions(int y, int a[], int mul[], int max[], int lenght, int solution
         }
     }
 
-
-    for (i = 0; i < lenght; i++) {
-        sum += a[i] * mul[i];
-    }
-
-    if (sum == y) {
+    /* to make the program more efficient, I don't go through all the possibilities, but check the condition for the multiple with the first indes */
+    if ((y - sum >= 0) && ((y-sum) % a[0] == 0)) {
         solutions++;
     }
 
     mul[lenght-1]++;
 
-    findSolutions(y, a, mul, max, lenght, solutions);
+    findSolutions(y, a, mul, max, lenght, solutions);    /* recursive call */
 }
 
 int main(int argc, char *argv[]) {
-    int n;
-    int y;
+    int n;    /* lenght from input */
+    int y;    /* desired sum */
     int solutions;
     
     scanf("%d:", &n);
@@ -71,21 +78,8 @@ int main(int argc, char *argv[]) {
 
     fillArray(array, n);
     scanf("%d",&y);
-
-    for (int i = 0; i < n; i++) {
-        multiples[i] = 0;
-    }
-
+    fillMultiples(multiples, n);
     fillMaxMultiples(maxMultiples, array, n, y);
-/*
-    for (int i = 0; i < n; i++) {
-        printf("%d ", array[i]);
-    }
-    printf("\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", maxMultiples[i]);
-    }
-    printf("\n"); */
 
     solutions = findSolutions(y, array, multiples, maxMultiples, n, 0);
 
