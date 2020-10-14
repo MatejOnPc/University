676f2d6ddaaead83d7170800bcda7c8879703972
Matej <matejxpriesol@gmail.com>
2020-10-14 13:05:42 +0200

    Fixed exer 3
    Fixed timeout error, basically redid the whole program

--- Year 1/Imperative Programming/week5/lab/linearequation/linearEquation.c
+++ Year 1/Imperative Programming/week5/lab/linearequation/linearEquation.c
@@ -1,7 +1,7 @@
 /* file    : linearEquation.c */
 /* author  : Matej Priesol (email: m.priesol@student.rug.nl) */
-/* date    : TUE Oct 13 2020 */
-/* version : 1.2 */
+/* date    : WED Oct 14 2020 */
+/* version : 1.3 */
 
 /* Description: This program finds number of possible solutions for a given linear equation */
 
@@ -17,51 +17,20 @@ void fillArray(int a[], int lenght) {
     }
 }
 
-/* fills array multiples */
-void fillMultiples(int a[], int lenght) {
-    for (int i = 0; i < lenght; i++) {
-        a[i] = 0;
-    }
-}
-
-/* fills maximum values for each integer, used for going through all the possible solutions */
-void fillMaxMultiples(int max[], int a[], int lenght, int y) {
-    for (int i = 0; i < lenght; i++) {
-        max[i] = y / a[i];
-    }
-}
-
 /* recursive funcion */
-int findSolutions(int y, int a[], int mul[], int max[], int lenght, int solutions) {
-    int sum = 0;
-    int i;
+int findSolutions(int y, int a[], int index, int multiple, int lenght) {
 
     /* base case */
-    if (mul[1] > max[1]) {
-        return solutions;
+    if ((y == 0) && (index == lenght)) {
+        return 1;
+    }
+    /* base cases */
+    if ((y < 0) || (index >= lenght) || (multiple*a[index]) > y) {
+        return 0;
     }
 
-    /* calculates sum */
-    for (i = 1; i < lenght; i++) {
-        sum += a[i] * mul[i];
-    }
-
-    /* makes sure I move through all possibilities */
-    for (i = 1; i < lenght; i++) {
-        if (mul[i] > max[i]) {
-            mul[i-1]++;
-            mul[i] = 0;
-            return findSolutions(y, a, mul, max, lenght, solutions);
-        }
-    }
-
-    /* to make the program more efficient, I don't go through all the possibilities, but check the condition for the first index of multiple */
-    if ((y - sum >= 0) && ((y-sum) % a[0] == 0)) {
-        solutions++;
-    }
-
-    mul[lenght-1]++;
-    findSolutions(y, a, mul, max, lenght, solutions);    /* recursive call */
+    /* recursive call, two options, I either move to the next index in array or I add one to the multiple and stay at the same index in array */
+    return findSolutions(y - multiple*a[index], a, index+1, 0, lenght) + findSolutions(y, a, index, multiple+1, lenght);
 }
 
 int main(int argc, char *argv[]) {
@@ -72,16 +41,10 @@ int main(int argc, char *argv[]) {
     scanf("%d:", &n);
 
     int array[n];
-    int multiples[n];
-    int maxMultiples[n];
-
     fillArray(array, n);
     scanf("%d",&y);
-    fillMultiples(multiples, n);
-    fillMaxMultiples(maxMultiples, array, n, y);
 
-    solutions = findSolutions(y, array, multiples, maxMultiples, n, 0);
-
+    solutions = findSolutions(y, array, 0, 0, n);
     printf("%d\n", solutions);
 
     return 0;
