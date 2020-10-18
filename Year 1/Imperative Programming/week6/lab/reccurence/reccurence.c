9d8d960d706ade5240f49ff03ea79a68b83277c4
Matej <matejxpriesol@gmail.com>
2020-10-18 13:15:43 +0200

    Finished exer 1
    Passed themis first try

--- Year 1/Imperative Programming/week6/lab/reccurence/reccurence.c
+++ Year 1/Imperative Programming/week6/lab/reccurence/reccurence.c
@@ -1,15 +1,47 @@
-/* file    : reccurance.c */
+/* file    : reccurence.c */
 /* author  : Matej Priesol (email: m.priesol@student.rug.nl) */
 /* date    : SUN Oct 18 2020 */
 /* version : 1.0 */
 
-/* Description:  */
+/* Description: This program reads the input n and outputs Cn, where Cn is defined by the reccurence in problem description */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+
+/* recursive function, returns the value of Cn */
+int reccurence(int n, int rM[]) {
+    /* base cases */
+    if (n <= 1) {
+        return n*2;
+    }
+
+    /* recursive case */
+    if (rM[n] == 0) {
+        /* reccurence(n) has not been computed before */
+        rM[n] = 4 * reccurence(n-1, rM) - reccurence(n-2, rM);
+    }
+
+    return rM[n];
+}
+
+
 int main(int argc, char *argv[]) {
-    
+    int n;
+    int output;
+    int reccurenceMembers[47];
+
+    /* fill the array with 0s */
+    for (int i = 0; i < 47; i++) {
+        reccurenceMembers[i] = 0;
+    }
+
+    scanf("%d", &n);
+
+    output = reccurence(n, reccurenceMembers);
+
+    printf("%d\n", output);
+
     return 0;
 }
