696f16ce4a91475d2dfba2430af1e29742ecca76
Matej <matejxpriesol@gmail.com>
2020-10-18 12:53:09 +0200

    added description

---
+++ Year 1/Imperative Programming/week6/lab/numberPyramids/numberPyramids.c
@@ -0,0 +1,15 @@
+/* file    : .c */
+/* author  : Matej Priesol (email: m.priesol@student.rug.nl) */
+/* date    : SUN Oct 18 2020 */
+/* version : 1.0 */
+
+/* Description:  */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
