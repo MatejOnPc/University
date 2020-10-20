4abb973ad692a869bb7d1372540f5f184d1f4d97
Matej <matejxpriesol@gmail.com>
2020-10-20 12:15:33 +0200

    Finished exer 4
    Couldn't fix the error, but tried completely different approach

--- Year 1/Imperative Programming/week6/lab/numberPyramids/numberPyramids.c
+++ Year 1/Imperative Programming/week6/lab/numberPyramids/numberPyramids.c
@@ -1,7 +1,7 @@
 /* file    : numberPyramids.c */
 /* author  : Matej Priesol (email: m.priesol@student.rug.nl) */
-/* date    : MON Oct 19 2020 */
-/* version : 1.0 */
+/* date    : TUE Oct 20 2020 */
+/* version : 1.3 */
 
 /* Description: This programs reads an array from input and outputs whether its possible to create a pyramid based on the rules given in problem description */
 
@@ -15,128 +15,73 @@ int *dynamicArray(int size) {
 	return array;
 }
 
-/* cretes new divided arrays to sort */
-int *copySubArray(int left, int right, int arr[]) {
-    int length = right - left;
-    int *copy = malloc(length*sizeof(int));
-    for (int i=0; i < length; i++) {
-        copy[i] = arr[i + left];
+/* this function finds whether there is a majority in the array */
+int hasMajority(int length, int a[]) {
+    int candidate, counter, surplus = 0;
+    int i;
+    int *newArray;    /* if there is a majority, I'll create new array without the element and pass it to the function again (so I'll check for all levels) */
+    int index = 0;
+
+    /* this loop finds candidate for a majority */
+    for (i=0; i < length; i++) {
+        if (surplus == 0) { /* there's a new candidate */
+            candidate = a[i];
+            surplus = 1;
+        } else { /* we have a candidate already */
+            if (a[i] == candidate) { /* another vote */
+            surplus++;
+            } else { /* cancel out votes */
+                surplus--;
+            }
+        }
     }
-    return copy;
-}
 
-void merge(int lenA, int a[], int lenB, int b[], int arr[]) {
-    int idx=0, l=0, r=0;
+    /* if there is a majority, then we know the candidate */
+    counter = 0;
+    for (i=0; i < length; i++) {
+        if (a[i] == candidate) {
+            counter++;
+        }
+    }
 
-    /* sorts arrays until one of them was fully used */
-    while ((l != lenA) && (r != lenB)) {
-        if (a[l] <= b[r]) {
-            arr[idx] = a[l];
-            l++;
+    /* if candidate has a majority, I create new array without the candidate and pass it to the function again */
+    if (2*counter > length) {    /* has a majority */
+        length = length - counter;    /* lenght of the new array */
+        newArray = dynamicArray(length);
+        /* fills the new array with all the elemnts that are not the candidate */
+        for (i = 0; i < length + counter; i++) {
+            if (a[i] != candidate) {
+                newArray[index] = a[i];
+                index++;
+            }
+        }
+        /* if lenght isn't zero, I call the function again with the new array. If it is, I return 1 and therefore print YES */
+        if (length != 0) {
+            return hasMajority(length, newArray);
         } else {
-            arr[idx] = b[r];
-            r++;
-        }
-        idx++;
-    }
-
-    /* adds the rest to the sorted array */
-    while (l != lenA) {
-        arr[idx] = a[l];
-        idx++;
-        l++;
-    }
-    while (r != lenB) {
-        arr[idx] = b[r];
-        idx++;
-        r++;
-    }
-}
-
-
-void mergeSort(int length, int arr[]) {
-    /* base case */
-    if (length <= 1) {
-        return;
-    }
-    /* recursive case */
-    int mid = length/2;
-
-    /* make two subarrays and sort recursively*/
-    int *left = copySubArray(0, mid, arr);
-    mergeSort(mid, left);
-    int *right = copySubArray(mid, length, arr);
-    mergeSort(length - mid, right);
-
-    /* merge sorted arrays */
-    merge(mid, left, length - mid, right, arr);
-
-    /* free auxiliary memory */
-    free(right);
-    free(left);
-}
-
-/* prints whether there is a solution or not */
-void solution(int lenght, int a[], int index, int sumSoFar) {
-    int sum;
-
-    /* base case, I went through all numbers in array */
-    if (index == lenght) {
-        printf("YES\n");
-        return;
-    }
-
-    if (a[index] == 0) {    /* I want to skip 0s at the begging of sorted array */
-        solution(lenght, a, index+1, sumSoFar);
-    } else {
-        sum = a[index];
-        if (sumSoFar < sum) {    /* based on the problem rules */
-            sumSoFar += a[index];
-            solution(lenght, a, index+1, sumSoFar);
-        } else {    /* If the rule was broken, I want to print NO and return */
-            printf("NO\n");
-            return;
+            return 1;
         }
     }
+    return 0;
 }
 
 int main(int argc, char *argv[]) {
-    int max = 0, i;
-    int *histogram;
+    int *array;
+    int nInArray;
     int lenght;
     scanf("%d:", &lenght);
 
-
     /* create an array with scanned lenght, fill with the numbers from input*/
-    int nInArray;
-    int array[lenght];
-    for (i = 0; i < lenght; i++) {
+    array = dynamicArray(lenght);
+    for (int i = 0; i < lenght; i++) {
         scanf("%d", &nInArray);
         array[i] = nInArray;
     }
 
-    /* this fucntion finds maximum value in array */
-    for (i = 0; i < lenght; i++) {
-        if (array[i] > max) {
-            max = array[i];
-        }
+    if (hasMajority(lenght, array)) {
+        printf("YES\n");
+    } else {
+        printf("NO\n");
     }
-
-    histogram = dynamicArray(max + 1);    /* I want to have index with value max */
-    for (i = 0; i < max+1; i++) {
-        histogram[i] = 0;
-    }
-
-    /* fill histogram with 0s */
-    for (i = 0; i < lenght; i++) {
-        histogram[array[i]]++;
-    }
-
-    mergeSort(max+1, histogram);    /* sort histogram */
-
-    solution(max+1, histogram, 0, 0);
-
-    free(histogram);
-
     return 0;
 }
